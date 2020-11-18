98a59bec65fa62008d2e2dde3747e4cff4556aa2
lzm0010 <lzm0010@gmail.com>
2020-11-18 11:22:43 -0600

    added airtable and get records

---
+++ pages/api/utils/Airtable.js
@@ -0,0 +1,17 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+const getMinifiedRecord = (record) => {
+  return {
+    id: record.id,
+    fields: record.fields
+  }
+}
+
+const minifyRecords = (records) => {
+  return records.map(record => getMinifiedRecord(record));
+}
+
+export {table, minifyRecords, getMinifiedRecord};
